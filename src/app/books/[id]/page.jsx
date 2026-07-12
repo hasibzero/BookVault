136257581a4bb134cca50274bc583371ccd6fbfc
Hasib <hasibsarkar98@gmail.com>
2026-07-12 22:44:10 +0600

    error fix

--- src/app/books/[id]/page.jsx
+++ src/app/books/[id]/page.jsx
@@ -1,4 +1,4 @@
-import { BorrowButton } from '@/components/books/BorrowButton';
+import BorrowButton from '@/components/books/BorrowButton';
 import Image from 'next/image';
 import Link from 'next/link';
 
