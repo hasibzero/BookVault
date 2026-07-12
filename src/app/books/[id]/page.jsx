35a981f26daaf6c13ea14226eb3d92b6c9ab5a82
Hasib <hasibsarkar98@gmail.com>
2026-07-12 20:43:11 +0600

    dependencies: install heroui toast modules

--- src/app/books/[id]/page.jsx
+++ src/app/books/[id]/page.jsx
@@ -125,7 +125,7 @@ const BookDetailsPage = async ({params}) => {
                   Available Quantity: {book.available_quantity} copies left
                 </div>
               </div>
-              <button onClick={handleBorrow} className="mouse-pointer w-full sm:w-auto bg-[#c85a2f] hover:bg-[#b04a23] text-white px-6 py-3 rounded-md font-bold transition-colors flex items-center justify-center">
+              <button onClick={handleBorrow} className="hover:mouse-pointer w-full sm:w-auto bg-[#c85a2f] hover:bg-[#b04a23] text-white px-6 py-3 rounded-md font-bold transition-colors flex items-center justify-center">
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                 </svg>
